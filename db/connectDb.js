2d2ed932378a251d736f42b3d58ee66999e50821
Tarsh Swarnkar <123285559+T1A0R3S2H@users.noreply.github.com>
2024-06-02 23:46:17 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,41 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = 'mongodb+srv://tarshswarnkar:2FlHRLRI5RsKppTb@sahyog.pkxzwkt.mongodb.net/?retryWrites=true&w=majority&appName=sahyog';
-
-if (!MONGODB_URI) {
-  throw new Error('MongoDB URI is not defined');
-}
-
-/**
- * Global is used to maintain a cached connection across hot reloads in development.
- * This prevents connections growing exponentially during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+    if (mongoose.connection.readyState >= 1) {
+        return;
+    }
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  console.log(`MongoDb Connected: ${cached.conn.connection.host}`);
-  return cached.conn;
-};
+    try {
+        const conn = await mongoose.connect('mongodb+srv://tarshswarnkar:2FlHRLRI5RsKppTb@sahyog.pkxzwkt.mongodb.net/?retryWrites=true&w=majority&appName=sahyog');
+        console.log(`MongoDb Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
 
 export default connectDB;
